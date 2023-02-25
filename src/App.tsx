2eb95441d09e88c8b177ee30980e0ac195e27532
Alex Blits <blits.alex@gmail.com>
2023-02-25 13:00:15 +0200

    HW-34

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Timer from './components/Timer';
 
 function App() {
-  return <Timer interval = {2000}/>
+  return <div style={{display: "flex", alignItems: "center", marginTop: "10vh", flexDirection:"column"}}>
+    <div style={{display: "flex", justifyContent: "space-around"}}>
+      <Timer timeZone='Asia/Urumqi' city="Beijing" interval={2000}/>
+      <Timer timeZone='Europe/London' city="London"/>
+    </div>
+    <div style={{display: "flex", justifyContent: "space-around"}}>
+      <Timer timeZone='Europe/Paris' city="Paris"/>
+      <Timer timeZone='Asia/Jerusalem' city="Jerusalem"/>
+    </div>
+  </div>
 }
 
+
 export default App;
+
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 type Props = {
-    interval?: number; // optional  - if parent doesn't set the interval then it will be 1000ms (line 12)
+    interval?: number; //?-> optional  - if parent doesn't set the interval then it will be 1000ms (line 12)
+    timeZone: string;
+    city: string;
+
 }
-const Timer: React.FC<Props> = ({interval})=> {   // FC functional  component
+const Timer: React.FC<Props> = ({interval, timeZone, city})=> {   // FC functional  component
     const [time, setTime] = React.useState(new Date());
     function tic(): void {
         console.log('kuku');
@@ -10,11 +13,11 @@ const Timer: React.FC<Props> = ({interval})=> {   // FC functional  component
     }
     useEffect(()=> {
         setInterval(tic, interval || 1000);
-        },[]);   // don't cal  'setInterval' when  'time' changes
+        },[]);   // [] -> don't call 'setInterval' when  'time' changes
     
     return <div style={{marginLeft: "10vw"}}>
-        <h3>Current Time</h3>
-        <label>{time.toLocaleTimeString()}</label>
+        <h3>Current Time in {city}</h3>
+        <label>{time.toLocaleTimeString([],{timeZone})}</label>
     </div>
 }
 export default Timer;
