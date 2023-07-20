9a9d1fe67e35c85b4387a8cedb9e33331e0790ae
Alexblits <96486642+Alexblits@users.noreply.github.com>
2023-07-20 23:45:54 +0300

    sprint1: initial setup CW38

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import LifeGameConfig from './config/LifeGameConfig.json'
-import Life from './components/Life'
+
 
 function App() {
 
   return <div  >
-    <Life dimension={LifeGameConfig.dimension} ticInterval={LifeGameConfig.tic}></Life>   
   </div>
 }
 export default App;
