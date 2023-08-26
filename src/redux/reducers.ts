fc8e8f44eb1039e7df0011951c244f8994e88060
Alex Blits <blits.alex@gmail.com>
2023-08-26 17:33:35 +0300

    HW_39

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -4,9 +4,9 @@ import { Course } from "../models/Course"
 import { ADD_COURSE_ACTION, REMOVE_COURSE_ACTION, UPDATE_COURSE_ACTION } from "./actions"
 import { coursesService } from "../config/service-config"
 
-
+const init: Course[] = coursesService.get(); // instead doing "courses  = []" inside coursesReducer to keep same initial state//
 export const coursesReducer: Reducer<Course[], PayloadAction<Course | number>> =
- (courses = [], action): Course[] => {
+ (courses = init, action): Course[] => {
      switch(action.type) {
          case ADD_COURSE_ACTION: coursesService.add(action.payload as Course); break;
          case REMOVE_COURSE_ACTION: coursesService.remove(action.payload as number); break;
--- src/util/useImitator.ts
+++ src/util/useImitator.ts
@@ -1,14 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"; 
 import { getRandomNumber } from "./random";
 import { ImitatorAction, imitatorActions } from "../config/imitator-config";
-import { addCourse, removeCourse } from "../redux/actions";
+import { addCourse, removeCourse, updateCourse } from "../redux/actions";
 import { getRandomCourse } from "./randomCourse";
 import { coursesReducer } from "../redux/reducers";
 import courseData from '../config/courseData.json';
+import { useEffect } from "react";
+import { Course } from "../models/Course";
+import { StateType } from "../redux/store"; 
 
+let courses: Course[];
 export function useImitator(){
+    courses = useSelector<StateType, Course[]>(state => state.courses)
     const dispatch = useDispatch();
-    setInterval(action, 2000);
+
+    useEffect(() => {
+        const intervalId = setInterval(action, 2000);
+        return () => clearInterval(intervalId);
+    }, []);
 
     function action(){
         const number = getRandomNumber(0, 100);
@@ -23,20 +32,37 @@ export function useImitator(){
             return dispatch(addCourse(getRandomCourse(courseData)));
         }
         function dispatchRemove(){
-            //TODO
+            console.log('removing')
+            if (courses.length !== 0){
+                const index = getRandomNumber(0, courses.length - 1);
+                dispatch(removeCourse(courses[index].id));
+                console.log(`removed course with id ${courses[index].id}`);
+            }            
         }
-        function dispatchUpdate(){
-            //TODO
+        function dispatchUpdate(){ 
+            console.log('updating');
+            if (courses.length !== 0){
+                const index = getRandomNumber(0, courses.length - 1);
+                const course = getRandomCourse(courseData);
+                course.id = courses[index].id; // the replaced couse id has to equal the new (generated 'course') id
+                dispatch(updateCourse(course))
+                console.log(`updated course with ID : ${courses[index].id}`)
+
+            }            
         }
     }    
 }
 
-function getAction(number: any): ImitatorAction {
-    for (let i = 0; i < imitatorActions.length; i++) {
-        if (number < imitatorActions[i].prob) {
-            return imitatorActions[i];
-        }
-    }
-    return imitatorActions[imitatorActions.length - 1];
+// function getAction(number: any): ImitatorAction {
+//     for (let i = 0; i < imitatorActions.length; i++) {
+//         if (number < imitatorActions[i].prob) {
+//             return imitatorActions[i];
+//         }
+//     }
+//     return imitatorActions[imitatorActions.length - 1];
+// }
+function getAction(num: number): ImitatorAction {
+    return imitatorActions.find(ia => num <= ia.prob) ?? imitatorActions[imitatorActions.length -1];
+  
 }
 
